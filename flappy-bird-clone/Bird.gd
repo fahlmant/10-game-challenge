ad1421b5281aa21a02d68ddfee67419f09df3867
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-11 21:10:43 -0700

    Start Flappy Bird
    - Creates main world scene
    - Creates Bird with sprite and collision body
    - Creates Bird script for gravity and jumping

---
+++ flappy-bird-clone/Bird.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const JUMP_VELOCITY = -400.0
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	
+	velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = JUMP_VELOCITY
+	
+	move_and_slide()

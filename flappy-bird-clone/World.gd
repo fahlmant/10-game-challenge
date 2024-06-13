6d5f489400cdf566728f401164c7e339e13a8a7b
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-12 20:28:32 -0700

    Migrate world to use a Pipes node that holds all existing pipes

--- flappy-bird-clone/World.gd
+++ flappy-bird-clone/World.gd
@@ -4,7 +4,9 @@ var current_score = 0
 var high_score = 0
 
 func _ready():
-	pass
+	for pipe in $Pipes.get_children():
+		pipe.hit.connect(_on_pipe_hit)
+		pipe.score.connect(_on_pipe_score)
 
 func add_pipe():
 	pass
@@ -14,18 +16,14 @@ func _process(delta):
 		pipe.position.x -= 2
 
 func _on_pipe_score():
-	print("Score")
 	current_score += 1
 	$CurrentScoreNumber.text = str(current_score)
 	if current_score > high_score:
 		$HighScoreNumber.text = str(current_score)
 
 func _on_pipe_hit():
-	print("Hit Pipe")
+	print("Game Over")
 
 func _on_end_area_entered(area):
 	if area.name == "PipeEnd":
 		area.get_parent().queue_free()
-		print("Area")
-	else:
-		print("Not area: " + area.name)

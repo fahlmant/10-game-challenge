f810fd541a242d0190a165448d046eae570563b9
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-15 08:32:35 -0700

    Refactor Paddle to have left half and right half for bouncing

--- breakout/Paddle.gd
+++ breakout/Paddle.gd
@@ -1,2 +1,13 @@
 extends CharacterBody2D
 
+var speed = 400
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("Right"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Left"):
+		velocity.x += 1 
+	
+	set_velocity(velocity*speed)
+	move_and_slide()

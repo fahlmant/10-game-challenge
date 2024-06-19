b9fd13924ebf201bd7b724fde25374d5e5e3e2e2
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-18 20:20:34 -0700

    Change number of bricks and distribution

--- breakout/World.gd
+++ breakout/World.gd
@@ -2,20 +2,13 @@ extends Node
 
 var brick_scene = preload("res://Brick.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	new_game()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func new_game():
-	pass
-	for i in range(0,8):
+	for i in range(0,12):
 		for j in range(0,10):
 			var brick = brick_scene.instantiate()
 			$Bricks.add_child(brick)
 			brick.position.x = 100 * i + 100
-			brick.position.y = 50 * j + 100
+			brick.position.y = 40 * j + 50

fed7facb42edaa479f0497ef1e30a97fa95d5586
fahlmant <taylor@bridgetowngamestudios.com>
2024-06-30 16:00:11 -0700

    Add Score and Lives lables, as well as logic to add score

--- breakout/World.gd
+++ breakout/World.gd
@@ -2,10 +2,15 @@ extends Node
 
 var brick_scene = preload("res://Brick.tscn")
 
+var score: int = 0
+var lives: int = 3
+
 func _ready() -> void:
 	new_game()
 
 func new_game():
+	score = 0
+	lives = 3
 	for i in range(0,12):
 		for j in range(0,10):
 			var brick = brick_scene.instantiate()
@@ -14,6 +19,8 @@ func new_game():
 			brick.hit.connect(_on_brick_hit)
 			$Bricks.call_deferred("add_child", brick)
 
-func _on_brick_hit(body):
+func _on_brick_hit(body): 
 	# Increase Score
+	score += 1
+	$ScoreNumber.text = str(score)
 	body.queue_free()

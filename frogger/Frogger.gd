7e62567aa4e704c94800d90bc62bd03cc3d42da0
fahlmant <taylor@bridgetowngamestudios.com>
2024-10-04 12:09:26 -0700

    Implement tile-based movement for frogger

--- frogger/Frogger.gd
+++ frogger/Frogger.gd
@@ -1,11 +1,20 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var tile_size = 32
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
 
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position += inputs[dir] * tile_size
